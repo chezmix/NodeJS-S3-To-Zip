4494b3fda9598ffcf4897eb3f1f2e6353a79d9fe
Ubuntu <ubuntu@ip-10-29-150-237.ec2.internal>
2012-07-20 22:02:56 +0000

    Pipe ZipStream directly to HTTP Response

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 NodeJS-S3-To-Zip
 ================
 
-An example app for zipping Amazon S3 files using the Readable Streams
+An example app for zipping Amazon S3 files using Readable Streams
--- zip.js
+++ zip.js
@@ -1,36 +1,64 @@
-//Example of using NodeJS Streams to zip files from Amazon S3
+//Example of using NodeJS Streams to zip files from Amazon S3 and then piping it to an HTTP Response
+var fs = require('fs');
 var settings = require('./settings');
 var s3 = require('aws2js').load('s3', settings.AWSAccessKeyId, settings.AWSSecretAccessKey);
 var ee = require('events').EventEmitter; //for event emitter errors
 var st = require('stream');
 var async = require('async');
 var zipstream = require('zipstream');
-var fs = require('fs');
-
 s3.setBucket(settings.S3Bucket);
-var files = [
-	{path:'testfolder/cg8.jpg', fileName:'cg8.jpg'},
-	{path:'testfolder/cg7.png', fileName:'cg7.png'}
-];
-	
-var out = fs.createWriteStream('out.zip');
-var zip = zipstream.createZip({ level: 1 });
-zip.pipe(out);
 
-async.forEachSeries(files, function(item, callback) { //need to call callback to denote when the async function is finished
-	s3.get(item.path, null, 'stream', function(err, response) {
-		if (err) {
-			console.log("err");
-			callback(err);
+//Returns the zipStream of files from S3.  
+//The cool thing about this is that the files are fetched asyncronously and we can use the ZipStream as we wish even if files are still being fetch from S3.
+
+function getZipStream() {
+	
+	var files = [
+		{path:'testfolder/cg8.jpg', fileName:'cg8.jpg'},
+		{path:'testfolder/cg7.png', fileName:'cg7.png'}
+	];
+		
+	var zip = zipstream.createZip({ level: -1 });
+	
+	async.forEachSeries(files, function(item, callback) { //need to call callback to denote when the async function is finished
+		console.log("Grabbing S3 File");
+		s3.get(item.path, null, 'stream', function(err, response) {
+			if (err) {
+				console.log("err");
+				callback(err);
+			} else {
+				zip.addFile(response, { name: item.fileName }, callback);
+			}
+		});
+	},
+	function (err) {
+		if (!err) {
+			console.log("Finalizing");
+			zip.finalize(function(written) { console.log(written + ' total bytes written'); });
 		} else {
-			zip.addFile(response, { name: item.fileName }, callback);
+			console.log(err.toString());
 		}
 	});
-},
-function (err) {
-	if (!err) {
-		zip.finalize(function(written) { console.log(written + ' total bytes written'); });
-	} else {
-		console.log(err.toString());
-	}
+	
+	//Files are probably sill being fetched at this point, we don't care!
+	return zip;
+}
+
+//In this example ZipStream is piped directly to HTTP Response.  No temp files are created!
+
+var http = require('http');
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {
+		'Content-type': 'application/octet-stream',
+		'Content-Disposition':'attachment;filename=out.zip'
+    });
+	
+	var zipStream = getZipStream();
+	zipStream.pipe(response);
+	zipStream.on('end', function() {
+		response.end();
+	});
 });
+
+server.listen(8000);
+console.log('Listening on Port 8000...');
